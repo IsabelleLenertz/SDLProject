9bb3a092934ea917143a7afcce0de47af11bc579
Isabelle L <anzu67@gmail.com>
2015-02-22 10:47:19 -0800

    Fixed linker configuration issues. Added a sample SDL code.

--- Src/main.cpp
+++ Src/main.cpp
@@ -16,15 +16,70 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
+#include "sdl.h"
+#include "SDL_image.h"
 
-#include "SDL/SDL.h"
+using namespace std;
+int main(int argc, char* argv[])
+{
+    SDL_Surface* pDisplaySurface = NULL; //display surface
+    SDL_Event event; //event structure
 
-int main( int argc, char* args[] ) {
-    //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
+    //initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO)==-1) {
+        cerr << "Could not initialize SDL!" << endl;
+        exit(1);
+    } else {
+        //report success
+        cout << "SDL initialized properly!" << endl;
+        }
+        //create the window
+        pDisplaySurface = SDL_SetVideoMode(1024,768,0,SDL_ANYFORMAT);
+        //error check
+        if (pDisplaySurface == NULL) {
+        //report error on the creation of video display
+        cerr << "Could not set up display surface!" << endl;
+        exit(1);
+    }
+    // Read in the image
+    SDL_Surface* pJpegimage = IMG_Load("C:\\tmp\\goat.jpg");
+    if (pJpegimage == NULL) {
+        // report error trying to read in image file
+        cerr << "Could not read image file" << endl;
+        exit(1);
+    }
+    // Get image ready for display on the screen
+    SDL_Surface* pDisplayFormat = SDL_DisplayFormat(pJpegimage);
+    // show on display screen
+    SDL_Rect DestR;
+    DestR.x = 0;
+    DestR.y = 0;
+    SDL_BlitSurface(pDisplayFormat, NULL, pDisplaySurface, &DestR);
 
-    //Quit SDL
+    // process events until user closes the window
+    for(;;) {
+        //wait for an event
+        if(SDL_WaitEvent(&event)==0) {
+            cerr << "Error while waiting for an event!" << endl;
+            exit(1);
+        }
+
+        //check for a quit event
+        if(event.type==SDL_QUIT) break;
+
+        //update the screen
+        SDL_UpdateRect(pDisplaySurface,0,0,0,0);
+    }
+    // unload the dynamically loaded image libraries
+    SDL_FreeSurface(pJpegimage);
+
+    IMG_Quit();
+
+    // close SDL
     SDL_Quit();
-
-    return 0;
+    // we are done
+    cout << "Terminating normally." << endl;
+    //return 0
+    return(0);
 }
